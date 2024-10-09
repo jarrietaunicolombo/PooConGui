29d66c5b9a7a4d7bcd089f578a2168bfab6570b6
John Carlos Arrieta Arrieta <johncarlosarrietaarrieta@gmail.com>
2024-10-09 17:58:29 -0500

    Se creó el proyecto sobre el IDE Netbeans y la clase Main

---
+++ src/co/edu/ucolombo/s3/poo/Main.java
@@ -0,0 +1,12 @@
+package co.edu.ucolombo.s3.poo;
+
+/**
+ *
+ * @author johnarrieta
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("PRUEBA DE MAIN");
+    }
+}
